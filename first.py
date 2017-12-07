4b0a3722a88255cba4c9472b4c51ff66e75137b5
Oleksandr Liemieshko <oliemieshko@mirantis.com>
2017-12-07 10:02:23 -0600

    secont line

--- first.py
+++ first.py
@@ -1,2 +1,14 @@
-f = "%r %r"
-print f % (1,2)
+cat = "\t I am tabbet in"
+pers = "I am pers"
+back = "I a, \\ a \\ cat"
+
+fat_Cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print cat
+print pers
+print back
